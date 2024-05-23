7b27c55fc09b24ab50cd4709aaecc5fed4035e98
Egor Poderiagin <egor@crazyrussian.pro>
2024-05-23 11:58:49 +0700

    tests: added tests for latest endpoint

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,29 @@
 from fastapi.testclient import TestClient
 
 from .main import app
+from .data import rates
 
 client = TestClient(app)
 
 
 
-def test_can_get_latest_rates():
+def test_empty_at_latest_rates_when_nothing_loaded():
     response = client.get('/latest')
+
     assert response.status_code == 200
-    assert response.json() == {
-        'base': 'EUR',
-        'rates': {
-            'USD': 1.1
-        }
-    }
+    assert response.json()['base'] == 'EUR'
+    assert response.json()['rates'] == []
+    assert response.json()['date'] is None
+
+
+
+def test_appears_what_loaded_at_latest_rates():
+    rates['USD'] = 1.0
+
+    response = client.get('/latest')
+
+    assert response.status_code == 200
+    assert response.json()['base'] == 'EUR'
+    assert response.json()['rates'] == [
+        {'currency': 'USD', 'rate': 1.0}
+    ]
