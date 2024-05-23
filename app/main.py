e16b1757bde7537b3cde47fde1efa4b440badb91
Egor Poderiagin <egor@crazyrussian.pro>
2024-05-23 12:21:56 +0700

    feat: allow filtering by multiple symbols at latest endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from typing import Union
+from typing import Annotated
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
 
 from .data import rates, latest_daily_rate_for
@@ -18,25 +18,36 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get('/', include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def redirect_docs():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url="/docs")
 
 
-@app.get('/info')
+@app.get("/info")
 def info():
-    return {
-        'base': 'EUR',
-        'daily_rates_for': latest_daily_rate_for
-    }
+    return {"base": "EUR", "daily_rates_for": latest_daily_rate_for}
 
 
-@app.get('/latest')
-def latest_rates() -> LatestResponse:
-    return LatestResponse(
-        rates=[Rate(currency=currency, rate=rate) for currency, rate in rates.items()],
-        date=latest_daily_rate_for
-    ) 
+@app.get("/latest")
+def latest_rates(
+    symbols: Annotated[list[str] | None, Query()] = None,
+) -> LatestResponse:
+    # if parameters passed as comma separated string, probably there's a better way to do same
+    if symbols and len(symbols) == 1 and "," in symbols[0]:
+        symbols = symbols[0].split(",")
+
+    if symbols:
+        rates_to_show = [
+            Rate(currency=symbol, rate=rates[symbol])
+            for symbol in symbols
+            if symbol in rates
+        ]
+    else:
+        rates_to_show = [
+            Rate(currency=currency, rate=rate) for currency, rate in rates.items()
+        ]
+    return LatestResponse(rates=rates_to_show, date=latest_daily_rate_for)
+
 
 # ToDo: make history endpoint
 # @app.get('/history')
--- app/test_main.py
+++ app/test_main.py
@@ -6,24 +6,46 @@ from .data import rates
 client = TestClient(app)
 
 
-
 def test_empty_at_latest_rates_when_nothing_loaded():
-    response = client.get('/latest')
+    response = client.get("/latest")
 
     assert response.status_code == 200
-    assert response.json()['base'] == 'EUR'
-    assert response.json()['rates'] == []
-    assert response.json()['date'] is None
-
+    assert response.json()["base"] == "EUR"
+    assert response.json()["rates"] == []
+    assert response.json()["date"] is None
 
 
 def test_appears_what_loaded_at_latest_rates():
-    rates['USD'] = 1.0
+    rates["USD"] = 1.0
 
-    response = client.get('/latest')
+    response = client.get("/latest")
 
     assert response.status_code == 200
-    assert response.json()['base'] == 'EUR'
-    assert response.json()['rates'] == [
-        {'currency': 'USD', 'rate': 1.0}
+    assert response.json()["base"] == "EUR"
+    assert response.json()["rates"] == [{"currency": "USD", "rate": 1.0}]
+
+
+def test_filter_by_symbol_at_latest_rates():
+    rates["USD"] = 1.0
+    rates["THB"] = 30.0
+
+    response = client.get("/latest?symbols=USD")
+
+    assert response.status_code == 200
+    assert response.json()["base"] == "EUR"
+    assert response.json()["rates"] == [{"currency": "USD", "rate": 1.0}]
+
+
+def test_filter_by_symbols_at_latest_rates():
+    rates["USD"] = 1.0
+    rates["THB"] = 30.0
+    rates["RUR"] = 70.0
+
+    response = client.get("/latest?symbols=USD,RUR")
+
+    assert response.status_code == 200
+    assert response.json()["base"] == "EUR"
+    assert response.json()["rates"] == [
+        {"currency": "USD", "rate": 1.0},
+        {"currency": "RUR", "rate": 70.0},
     ]
